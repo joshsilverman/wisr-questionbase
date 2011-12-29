1f9075d45954e2ea8afd56f54c0eacd47e64b949
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-12-29 11:22:32 -0500

    PGerror fix attempt 3

--- app/controllers/api_v1_controller.rb
+++ app/controllers/api_v1_controller.rb
@@ -13,7 +13,7 @@ class ApiV1Controller < ApplicationController
   
   def get_lessons
     @lesson_ids = params[:ids].split('+')
-    book_ids = Chapter.where(:id => @lesson_ids).group("chapters.book_id, chapters.id").collect(&:book_id)
+    book_ids = Chapter.select(:book_id).where(:id => @lesson_ids).group("chapters.book_id, chapters.id").collect(&:book_id)
     @books = Book.where(:id => book_ids)
     @chapter = Chapter.includes(:book).where(:id => @lesson_ids)
     puts @chapter.inspect
