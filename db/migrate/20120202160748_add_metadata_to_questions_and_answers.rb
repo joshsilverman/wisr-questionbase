76e1469f80601f1c28270a82a27ea5261a6e7697
Jason Urton <jason.urton@gmail.com>
2012-02-06 12:27:38 -0500

    mod migration so not irreversible

--- db/migrate/20120202160748_add_metadata_to_questions_and_answers.rb
+++ db/migrate/20120202160748_add_metadata_to_questions_and_answers.rb
@@ -8,10 +8,10 @@ class AddMetadataToQuestionsAndAnswers < ActiveRecord::Migration
     add_column :questions, :state_objective, :string
     add_column :questions, :state, :string
     add_column :questions, :question_type, :string
-    remove_column :questions, :correct_answer
-    remove_column :questions, :incorrect_answer1
-    remove_column :questions, :incorrect_answer2
-    remove_column :questions, :incorrect_answer3
+    # remove_column :questions, :correct_answer
+    # remove_column :questions, :incorrect_answer1
+    # remove_column :questions, :incorrect_answer2
+    # remove_column :questions, :incorrect_answer3
 
     add_column :answers, :feedback, :string 
     
