a81144e756706627220e86d750d79a7e4a59d207
Bill DeRusha <bill.derusha@gmail.com>
2012-02-27 22:51:20 -0500

    migrate fix

--- db/migrate/20111214044133_create_answers.rb
+++ db/migrate/20111214044133_create_answers.rb
@@ -1,10 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration
     def change
-        create_table :answers do |t|
-	        t.integer :question_id
-	        t.text :answer
-	        t.boolean :correct
-	        t.timestamps
-        end
+        # create_table :answers do |t|
+	       #  t.integer :question_id
+	       #  t.text :answer
+	       #  t.boolean :correct
+	       #  t.timestamps
+        # end
     end
 end
