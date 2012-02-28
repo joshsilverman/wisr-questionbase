340cfc14991415971986be1b039a4c0d1b2201af
Bill DeRusha <bill.derusha@gmail.com>
2012-02-27 23:20:32 -0500

    migrate fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
 
   protected
 
     def login_required
       authenticate_user!
     end
-
-    # def current_user
-    #   session[:user]
-    # end
 end
