6e8bf2864b4b93701e9e31f8c75dc0c4a0fbdde5
Jason Urton <jason.urton@gmail.com>
2012-03-03 11:37:54 -0500

    published migration, view

--- db/schema.rb
+++ db/schema.rb
@@ -11,58 +11,96 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120221013751) do
+ActiveRecord::Schema.define(:version => 20120303162635) do
+
+  create_table "answers", :force => true do |t|
+    t.integer   "question_id"
+    t.text      "answer"
+    t.boolean   "correct"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.text      "feedback"
+  end
 
   create_table "books", :force => true do |t|
-    t.string   "name"
-    t.string   "author"
-    t.integer  "edition"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "public",     :default => false
-    t.integer  "user_id"
+    t.string    "name"
+    t.string    "author"
+    t.integer   "edition"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.boolean   "public",     :default => false
+    t.integer   "user_id"
   end
 
   create_table "chapters", :force => true do |t|
-    t.string   "name"
-    t.integer  "number"
-    t.integer  "book_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "name"
+    t.integer   "number"
+    t.integer   "book_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.boolean   "published",  :default => false
+  end
+
+  create_table "keywords", :force => true do |t|
+    t.string    "keyword"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+  end
+
+  create_table "keywords_questions", :id => false, :force => true do |t|
+    t.integer "keyword_id"
+    t.integer "question_id"
   end
 
   create_table "questions", :force => true do |t|
-    t.text     "question",          :limit => 255
-    t.string   "topic"
-    t.string   "correct_answer"
-    t.string   "incorrect_answer1"
-    t.string   "incorrect_answer2"
-    t.string   "incorrect_answer3"
-    t.integer  "user_id"
-    t.integer  "chapter_id"
-    t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text      "question"
+    t.string    "topic"
+    t.integer   "user_id"
+    t.integer   "chapter_id"
+    t.integer   "score"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.text      "rationale"
+    t.string    "difficulty"
+    t.string    "reference"
+    t.text      "objective"
+    t.string    "state_objective"
+    t.string    "state"
+    t.string    "question_type"
+  end
+
+  create_table "resources", :force => true do |t|
+    t.text      "url"
+    t.string    "media_type"
+    t.boolean   "contains_answer"
+    t.integer   "begin"
+    t.integer   "end"
+    t.integer   "question_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.text      "article_text"
+    t.text      "table"
+    t.boolean   "required"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "school"
-    t.string   "user_type"
-    t.string   "user_token"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "first_name"
+    t.string    "last_name"
+    t.string    "school"
+    t.string    "user_type"
+    t.string    "user_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
