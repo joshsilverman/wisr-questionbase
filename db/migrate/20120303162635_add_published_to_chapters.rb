5051590aa32a9cf25833f27d984082f5d33cd374
Jason Urton <jason.urton@gmail.com>
2012-03-08 12:02:57 -0500

    migration published default val to boolean

--- db/migrate/20120303162635_add_published_to_chapters.rb
+++ db/migrate/20120303162635_add_published_to_chapters.rb
@@ -1,5 +1,5 @@
 class AddPublishedToChapters < ActiveRecord::Migration
   def change
-    add_column :chapters, :published, :boolean, :default => 0
+    add_column :chapters, :published, :boolean, :default => false
   end
 end
