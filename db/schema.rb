7724d7066fb8c291861cc89b77f0fad4bba21d64
Bill DeRusha <bill.derusha@gmail.com>
2012-03-19 11:17:34 -0400

    removed old migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120303162635) do
+ActiveRecord::Schema.define(:version => 20120308201604) do
 
   create_table "answers", :force => true do |t|
     t.integer   "question_id"
