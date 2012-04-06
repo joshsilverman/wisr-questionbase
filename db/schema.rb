458144cf570852781af27eb1455d837d9fa4a254
Jason Urton <jason.urton@gmail.com>
2012-04-05 20:07:49 -0400

    added iframe

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@
 ActiveRecord::Schema.define(:version => 20120320173935) do
 
   create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "namespace"
+    t.string    "resource_id",   :null => false
+    t.string    "resource_type", :null => false
+    t.integer   "author_id"
+    t.string    "author_type"
+    t.text      "body"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "namespace"
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
@@ -29,57 +29,67 @@ ActiveRecord::Schema.define(:version => 20120320173935) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "answers", :force => true do |t|
-    t.integer  "question_id"
-    t.text     "answer"
-    t.boolean  "correct"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "feedback"
+    t.integer   "question_id"
+    t.text      "answer"
+    t.boolean   "correct"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.text      "feedback"
   end
 
   create_table "books", :force => true do |t|
-    t.string   "name"
-    t.string   "author"
-    t.integer  "edition"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "public",     :default => false
-    t.integer  "user_id"
+    t.string    "name"
+    t.string    "author"
+    t.integer   "edition"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.boolean   "public",     :default => false
+    t.integer   "user_id"
   end
 
   create_table "chapters", :force => true do |t|
-    t.string   "name"
-    t.integer  "number"
-    t.integer  "book_id"
+    t.string    "name"
+    t.integer   "number"
+    t.integer   "book_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "status",     :default => 0
+    t.text      "media_url"
+    t.integer   "author_id"
+  end
+
+  create_table "invoices", :force => true do |t|
+    t.integer  "user_id"
+    t.float    "hours"
+    t.text     "task"
+    t.boolean  "paid",       :default => false
+    t.float    "cost"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "status",     :default => 0
-    t.text     "media_url"
-    t.integer  "author_id"
   end
 
   create_table "keywords", :force => true do |t|
-    t.string   "keyword"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "keyword"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "keywords_questions", :id => false, :force => true do |t|
@@ -88,54 +98,54 @@ ActiveRecord::Schema.define(:version => 20120320173935) do
   end
 
   create_table "questions", :force => true do |t|
-    t.text     "question",        :limit => 255
-    t.string   "topic"
-    t.integer  "user_id"
-    t.integer  "chapter_id"
-    t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "rationale"
-    t.string   "difficulty"
-    t.string   "reference"
-    t.text     "objective"
-    t.string   "state_objective"
-    t.string   "state"
-    t.string   "question_type"
+    t.text      "question"
+    t.string    "topic"
+    t.integer   "user_id"
+    t.integer   "chapter_id"
+    t.integer   "score"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.text      "rationale"
+    t.string    "difficulty"
+    t.string    "reference"
+    t.text      "objective"
+    t.string    "state_objective"
+    t.string    "state"
+    t.string    "question_type"
   end
 
   create_table "resources", :force => true do |t|
-    t.integer  "question_id"
-    t.text     "url",             :limit => 255
-    t.boolean  "contains_answer"
-    t.string   "media_type"
-    t.integer  "begin"
-    t.integer  "end"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "article_text"
-    t.text     "table"
-    t.boolean  "required"
+    t.text      "url"
+    t.string    "media_type"
+    t.boolean   "contains_answer"
+    t.integer   "begin"
+    t.integer   "end"
+    t.integer   "question_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.text      "article_text"
+    t.text      "table"
+    t.boolean   "required"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "school"
-    t.string   "user_type"
-    t.string   "user_token"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "first_name"
+    t.string    "last_name"
+    t.string    "school"
+    t.string    "user_type"
+    t.string    "user_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
