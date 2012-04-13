57a5385816b3fa561a5b026cefaedec556d3f57b
Jason Urton <jason.urton@gmail.com>
2012-04-13 17:27:01 -0400

    fixes for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,34 @@
 book = Book.create!(:name => 'Book #1', :author => 'StudyEgg', :edition => 1, :public => true, :user_id => 1)
 
 book2 = Book.create!(:name => 'Book #2', :author => 'StudyEgg', :edition => 1, :public => true, :user_id => 1)
-book2.chapters << Chapter.create!(:book_id => book2.id, :name => 'Chapter #1', :number => 1, :status => 3)
 
 chapters = []
 chapters << Chapter.create!(:book_id => book.id, :name => 'Chapter #1', :number => 1, :status => 3)
 chapters << Chapter.create!(:book_id => book.id, :name => 'Chapter #2', :number => 2, :status => 3)
+chapters << Chapter.create!(:book_id => book.id, :name => 'Chapter #3', :number => 3, :status => 3)
+chapters << Chapter.create!(:book_id => book.id, :name => 'Chapter #4', :number => 4, :status => 3)
+chapters << Chapter.create!(:book_id => book.id, :name => 'Chapter #5', :number => 5, :status => 3)
+
+demo_chapter = Chapter.new
+demo_chapter.book_id = book.id
+demo_chapter.name = 'Chapter #5'
+demo_chapter.number = 6
+demo_chapter.status = 3
+demo_chapter.id = 112
+demo_chapter.save
+chapters << demo_chapter
+
+book2.chapters << Chapter.create!(:book_id => book2.id, :name => 'Chapter #1', :number => 1, :status => 3)
+
+strengths = []
+for k in 1..3
+	strengths << Keyword.create!(:keyword => "Strong Keyword ##{k}")
+end
+
+weaknesses = []
+for k in 1..3
+	weaknesses << Keyword.create!(:keyword => "Weak Keyword ##{k}")
+end
 
 chapters.each do |chapter|
 	for i in 1..20
@@ -17,10 +40,16 @@ chapters.each do |chapter|
 		for a in 1..3
 			q.answers << Answer.create!(:question_id => q.id, :answer => "Incorrect ##{a}", :correct => false)
 		end	
-		for k in 1..3
-			q.keywords << Keyword.create!(:keyword => "Keyword ##{k}")
+		if (i % 2 == 0)
+			for keyword in strengths
+				q.keywords << keyword
+			end
+		else
+			for keyword in weaknesses
+				q.keywords << keyword
+			end
 		end
-	  q.resources << 	Resource.create!(:question_id => q.id,
+	  q.resources << Resource.create!(:question_id => q.id,
 			:url => 'http://weeklyguiltypleasure.files.wordpress.com/2011/07/rick-astley.jpg',
 			:contains_answer => false,
 			:media_type => 'image',
